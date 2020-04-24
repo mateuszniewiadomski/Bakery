ab3997c76b13ebdb35c5280b685c9ca3addc2c8b
Mateusz Niewiadomski <matt_nie@wp.pl>
2020-04-24 15:33:19 +0200

    MSSQL DB Update

---
+++ BakeryDataBase.sql
@@ -0,0 +1,84 @@
+CREATE TABLE Position (
+	Id INTEGER PRIMARY KEY,
+	PositionName VARCHAR(30) UNIQUE NOT NULL,
+	Salary DECIMAL(8, 2) CHECK(Salary > 0) NOT NULL
+);
+CREATE TABLE Account (
+	Id INTEGER PRIMARY KEY,
+	Account VARCHAR(30) UNIQUE NOT NULL,
+	Password VARCHAR(100) NOT NULL 
+);
+CREATE TABLE Adres (
+	Id INTEGER PRIMARY KEY,
+	Street VARCHAR(30) NOT NULL,
+	Nr_Home INTEGER CHECK(Nr_Home > 0),
+	Nr_Apartment INTEGER CHECK(Nr_Apartment > 0),
+	PostalCode CHAR(6) NOT NULL,
+	City VARCHAR(30) NOT NULL
+);
+CREATE TABLE Employee (
+	Id INTEGER PRIMARY KEY,
+	Id_Adres INTEGER REFERENCES Adres(Id),
+	Id_Position INTEGER REFERENCES Position(Id),
+	Name VARCHAR(30) NOT NULL,
+	Surname VARCHAR(30) NOT NULL,
+	Pesel CHAR(11) NOT NULL,
+	BirthDate DATE CHECK(BirthDate <= GETDATE()) NOT NULL,
+	Gender CHAR(1) CHECK(Gender = 'w' OR Gender = 'm') NOT NULL,
+	PhoneNumber VARCHAR(9) NOT NULL,
+	Id_Account INTEGER REFERENCES Account(Id)
+);
+CREATE TABLE Customer (
+	Id INTEGER PRIMARY KEY,
+	Id_Adres INTEGER REFERENCES Adres(Id),
+	Name VARCHAR(30) NOT NULL,
+	Surname VARCHAR(30) NOT NULL,
+	Pesel CHAR(11) NOT NULL
+);
+CREATE TABLE Supplier (
+	Id INTEGER PRIMARY KEY,
+	Id_Adres INTEGER REFERENCES Adres(Id),
+	CompanyName VARCHAR(30) NOT NULL,
+	PhoneNumber VARCHAR(9) NOT NULL,
+	EMail CHAR(30) NOT NULL
+);
+CREATE TABLE Subcategory (
+	Id INTEGER PRIMARY KEY,
+	SubcategoryName VARCHAR(30) UNIQUE NOT NULL
+);
+CREATE TABLE Category (
+	Id INTEGER PRIMARY KEY,
+	CategoryName VARCHAR(30) UNIQUE NOT NULL,
+	Id_Subccategory INTEGER REFERENCES Subcategory(Id)
+);
+CREATE TABLE Product (
+	Id INTEGER PRIMARY KEY,
+	PoductName VARCHAR(30) UNIQUE NOT NULL,
+	Id_Category INTEGER REFERENCES Category(Id),
+	Price DECIMAL(8, 2) CHECK(Price > 0) NOT NULL,
+	Amount INTEGER CHECK(Amount > 0) NOT NULL,
+	Desctiprion TEXT,
+	Composition TEXT,
+	Id_Baker INTEGER REFERENCES Employee(Id),
+	Id_Supplier INTEGER REFERENCES Supplier(Id)
+);
+CREATE TABLE Packing (
+	Id INTEGER PRIMARY KEY,
+	PackingType VARCHAR(30) UNIQUE NOT NULL
+);
+CREATE TABLE Payment (
+	Id INTEGER PRIMARY KEY,
+	PaymentType VARCHAR(30) UNIQUE NOT NULL
+);
+CREATE TABLE PurchaseOrder (
+	Id INTEGER PRIMARY KEY,
+	Id_Customer INTEGER REFERENCES Customer(Id),
+	Id_Cashier INTEGER REFERENCES Employee(Id),
+	Id_PaymentType INTEGER REFERENCES Payment(Id),
+	Id_PackingType INTEGER REFERENCES Packing(Id),
+	ProductList TEXT NOT NULL,
+	OrderTime DATE NOT NULL,
+	OrderCompletion DATE NOT NULL,
+	OrderCost DECIMAL(8, 2) CHECK(OrderCost > 0) NOT NULL,
+	OrderCostWithTaxes DECIMAL(8, 2) CHECK(OrderCost > 0) NOT NULL
+);
